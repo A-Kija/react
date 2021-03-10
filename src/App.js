20559cca956539b5bc2b88cc316ff62022421f58
A-Kija <arvydas.kijakauskas@gmail.com>
2021-03-10 20:38:12 +0200

    begin

--- src/App.js
+++ src/App.js
@@ -1,22 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function BoxOne() {// <---- irg komponentas
+  return (
+    <div className="box">1</div>
+  );
+}
+
+const BoxTwo = () => {// <---- irg komponentas
+  return (
+    <div className="box">2</div>
+  );
+}
+
+const BoxThree = () => {// <---- irg komponentas
+  return (
+    <div className="box">3</div>
+  );
+}
+
+const BoxRandom = () => {// <---- irg komponentas
+  return (
+    <div className="box">
+      <span style={fancyStyle}>
+        {getRandomInt(10)}
+      </span>
+    </div>
+  );
+}
+
+const fancyStyle = {
+  fontSize: '300%',
+  color: 'yellow'
+}
+
+const normalStyle = {
+  fontSize: '100%',
+  color: 'white'
+}
+
+
+const BoxDigit = props => {// <---- irg komponentas
+  return (
+    <span style={props.boxStyle}>
+      {props.number}
+    </span>
+  );
+}
+
+
+const BoxUniversalum = props => {// <---- irg komponentas
+  return (
+    <div className="box" style={{borderColor:props.color}}>
+      <BoxDigit number={props.number} boxStyle={props.boxStyle} />
+    </div>
+  );
+}
+
+
+function App() { // <---- komponentas
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <BoxUniversalum color={'black'} number={1} boxStyle={normalStyle} />
+        <BoxUniversalum color={'black'} number={2} boxStyle={normalStyle} />
+        <BoxUniversalum color={'yellow'} number={3} boxStyle={normalStyle} />
+        <BoxUniversalum color={'green'} number={getRandomInt(10)} boxStyle={fancyStyle} />
       </header>
     </div>
   );
